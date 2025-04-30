601f9c04975a8a92723a29ea7982cb2a6434c3a5
Gridloch <margaretwalker999@gmail.com>
2025-04-30 17:23:17 +0100

    Update UI

--- public/js/loadscreen.js
+++ public/js/loadscreen.js
@@ -10,7 +10,7 @@ class Load extends Phaser.Scene
 
     preload ()
     {
-        // this.load.image('card_back', './images/selector/card_back.png');
+        this.load.image('background', './images/FantasyBG.png');
     }
 
     create ()
